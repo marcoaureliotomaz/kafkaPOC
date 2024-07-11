e1f66a2396b7085c0c96421aada5a47b1ef0c852
marco <marcoaureliotomaz@gmail.com>
2024-07-11 16:46:29 -0300

    Produtor OK

--- src/main/java/br/com/alura/ecommerce/NewOrderMain.java
+++ src/main/java/br/com/alura/ecommerce/NewOrderMain.java
@@ -14,7 +14,13 @@ public class NewOrderMain {
         var producer = new KafkaProducer<String,String>(properties());
         var value = "123,456,789";
         var record = new ProducerRecord<String, String>("ECOMMERCE_NEW_ORDER",value, value);
-        producer.send(record).get();
+        producer.send(record, (data, ex)->{
+          if (ex !=null) {
+            ex.printStackTrace();
+            return;
+          }
+          System.out.println(data.topic()+":::"+data.partition() + "/" + data.offset() + "/" + data.timestamp());
+        }).get();
     }
 
     private static Properties  properties(){
