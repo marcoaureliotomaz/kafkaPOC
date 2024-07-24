b9289ca7fcea23b35091c29cebf681a423dc9e32
marco <marcoaureliotomaz@gmail.com>
2024-07-23 23:00:43 -0300

    Paralelizando tarefas em um serviço

--- src/main/java/br/com/alura/ecommerce/NewOrderMain.java
+++ src/main/java/br/com/alura/ecommerce/NewOrderMain.java
@@ -1,26 +1,43 @@
 package br.com.alura.ecommerce;
 
+import org.apache.kafka.clients.producer.Callback;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.common.serialization.StringSerializer;
 
 import java.util.Properties;
+import java.util.UUID;
 import java.util.concurrent.ExecutionException;
 
 public class NewOrderMain {
 
     public static void main(String[] args) throws ExecutionException, InterruptedException {
         var producer = new KafkaProducer<String,String>(properties());
-        var value = "123,456,789";
+        var key = UUID.randomUUID().toString();
+        var value = "123,456,789" + key;
         var record = new ProducerRecord<String, String>("ECOMMERCE_NEW_ORDER",value, value);
-        producer.send(record, (data, ex)->{
-          if (ex !=null) {
-            ex.printStackTrace();
-            return;
-          }
-          System.out.println(data.topic()+":::"+data.partition() + "/" + data.offset() + "/" + data.timestamp());
-        }).get();
+        Callback callback = (data, ex) -> {
+            if (ex != null) {
+                ex.printStackTrace();
+                return;
+            }
+            System.out.println(data.topic() + ":::" + data.partition() + "/" + data.offset() + "/" + data.timestamp());
+        };
+        var email = "Obrigado pelo seu pedido! Estamos processando-o";
+        var emailRecord = new ProducerRecord<>("ECOMMERCE_SEND_EMAIL", email, email);
+        producer.send(record, callback).get();
+        producer.send(emailRecord, callback).get();
+
+
+
+
+
+
+
+
+
+
     }
 
     private static Properties  properties(){
